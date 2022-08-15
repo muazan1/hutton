32d0fe9390063f84e78f9896335c35489681a4d3
Muazan <muazan.stallyons@gmail.com>
2022-08-15 19:30:38 +0500

    Update HuttonRolePermissionsSeeder.php

--- src/publishable/database/seeders/HuttonRolePermissionsSeeder.php
+++ src/publishable/database/seeders/HuttonRolePermissionsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class RolePermissions extends Seeder
+class HuttonRolePermissionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
