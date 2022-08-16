b66ad6b2b464ea41e5208762b83ad055c1e9622a
Muazan <muazan.stallyons@gmail.com>
2022-08-16 13:33:12 +0500

    Update HuttonPermissionsSeeder.php

--- src/publishable/database/seeders/HuttonPermissionsSeeder.php
+++ src/publishable/database/seeders/HuttonPermissionsSeeder.php
@@ -89,16 +89,200 @@ class HuttonPermissionsSeeder extends Seeder
             ],
 
             // Builders module
+            [
+                'id' => 60,
+                'name' => 'Browse Builder',
+                'module' => 'builders',
+                'permission' => 'browse',
+                'api_permission' => 'builders:browse',
+            ],
+            [
+                'id' => 61,
+                'name' => 'Add Builder',
+                'module' => 'builders',
+                'permission' => 'create',
+                'api_permission' => 'builders:create',
+            ],
+            [
+                'id' => 62,
+                'name' => 'Edit Builder',
+                'module' => 'builders',
+                'permission' => 'edit',
+                'api_permission' => 'builders:edit',
+            ],
+            [
+                'id' => 63,
+                'name' => 'Delete Builder',
+                'module' => 'builders',
+                'permission' => 'delete',
+                'api_permission' => 'builders:delete',
+            ],
 
             // Sites Module module
-
+            [
+                'id' => 64,
+                'name' => 'Browse Sites',
+                'module' => 'sites',
+                'permission' => 'browse-site',
+                'api_permission' => 'sites:browse',
+            ],
+            [
+                'id' => 65,
+                'name' => 'Add Sites',
+                'module' => 'sites',
+                'permission' => 'create',
+                'api_permission' => 'sites:create',
+            ],
+            [
+                'id' => 66,
+                'name' => 'Edit Sites',
+                'module' => 'sites',
+                'permission' => 'edit',
+                'api_permission' => 'sites:edit',
+            ],
+            [
+                'id' => 67,
+                'name' => 'Delete Sites',
+                'module' => 'sites',
+                'permission' => 'delete',
+                'api_permission' => 'sites:delete',
+            ],
             // Building types module
-
+            [
+                'id' => 64,
+                'name' => 'Browse Building Types',
+                'module' => 'building_types',
+                'permission' => 'browse',
+                'api_permission' => 'building_types:browse',
+            ],
+            [
+                'id' => 65,
+                'name' => 'Add Building Types',
+                'module' => 'building_types',
+                'permission' => 'create',
+                'api_permission' => 'building_types:create',
+            ],
+            [
+                'id' => 66,
+                'name' => 'Edit Building Types',
+                'module' => 'building_types',
+                'permission' => 'edit',
+                'api_permission' => 'building_types:edit',
+            ],
+            [
+                'id' => 67,
+                'name' => 'Delete Building Types',
+                'module' => 'building_types',
+                'permission' => 'delete',
+                'api_permission' => 'building_types:delete',
+            ],
             // Plots module
+            [
+                'id' => 64,
+                'name' => 'Browse Plots',
+                'module' => 'plots',
+                'permission' => 'browse',
+                'api_permission' => 'building_types:browse',
+            ],
+            [
+                'id' => 65,
+                'name' => 'Add Plots',
+                'module' => 'plots',
+                'permission' => 'create',
+                'api_permission' => 'building_types:create',
+            ],
+            [
+                'id' => 66,
+                'name' => 'Edit Plots',
+                'module' => 'plots',
+                'permission' => 'edit',
+                'api_permission' => 'building_types:edit',
+            ],
+            [
+                'id' => 67,
+                'name' => 'Delete Plots',
+                'module' => 'plots',
+                'permission' => 'delete',
+                'api_permission' => 'building_types:delete',
+            ],
 
             // Service Pricings module
+            [
+                'id' => 64,
+                'name' => 'Browse Service Pricing',
+                'module' => 'service_pricing',
+                'permission' => 'browse',
+                'api_permission' => 'service_pricing:browse',
+            ],
+            [
+                'id' => 65,
+                'name' => 'Add Service Pricing',
+                'module' => 'service_pricing',
+                'permission' => 'create',
+                'api_permission' => 'service_pricing:create',
+            ],
 
             // joiners Pricings module
+            [
+                'id' => 64,
+                'name' => 'Browse Joiner Pricing',
+                'module' => 'joiner_pricing',
+                'permission' => 'browse',
+                'api_permission' => 'joiner_pricing:browse',
+            ],
+            [
+                'id' => 65,
+                'name' => 'Add Joiner Pricing',
+                'module' => 'joiner_pricing',
+                'permission' => 'create',
+                'api_permission' => 'joiner_pricing:create',
+            ],
+
+            // Jobs module
+            [
+                'id' => 54,
+                'name' => 'Jobs',
+                'module' => 'jobs',
+                'permission' => 'joiner-jobs',
+                'api_permission' => 'joiner:jobs',
+            ],
+            [
+                'id' => 55,
+                'name' => 'Admin Jobs',
+                'module' => 'jobs',
+                'permission' => 'admin-jobs',
+                'api_permission' => 'admin:jobs',
+            ],
+
+            // Services module
+            [
+                'id' => 64,
+                'name' => 'Browse Services',
+                'module' => 'services',
+                'permission' => 'browse',
+                'api_permission' => 'services:browse',
+            ],
+            [
+                'id' => 65,
+                'name' => 'Add Services',
+                'module' => 'services',
+                'permission' => 'create',
+                'api_permission' => 'services:create',
+            ],
+            [
+                'id' => 66,
+                'name' => 'Edit Services',
+                'module' => 'services',
+                'permission' => 'edit',
+                'api_permission' => 'services:edit',
+            ],
+            [
+                'id' => 67,
+                'name' => 'Delete Services',
+                'module' => 'services',
+                'permission' => 'delete',
+                'api_permission' => 'services:delete',
+            ],
 
             // work module
             [
@@ -124,81 +308,6 @@ class HuttonPermissionsSeeder extends Seeder
             ],
 
             /* permissions end here */
-
-            [
-                'id' => 54,
-                'name' => 'Jobs',
-                'module' => 'jobs',
-                'permission' => 'joiner-jobs',
-                'api_permission' => 'joiner:jobs',
-            ],
-            [
-                'id' => 55,
-                'name' => 'Admin Jobs',
-                'module' => 'jobs',
-                'permission' => 'admin-jobs',
-                'api_permission' => 'admin:jobs',
-            ],
-
-            // Builders Module
-            [
-                'id' => 60,
-                'name' => 'Browse Builder',
-                'module' => 'builders',
-                'permission' => 'browse-builder',
-                'api_permission' => 'builder:browse',
-            ],
-            [
-                'id' => 61,
-                'name' => 'Add Builder',
-                'module' => 'builders',
-                'permission' => 'add-builder',
-                'api_permission' => 'builder:create',
-            ],
-            [
-                'id' => 62,
-                'name' => 'Edit Builder',
-                'module' => 'builders',
-                'permission' => 'edit-builder',
-                'api_permission' => 'builder:edit',
-            ],
-            [
-                'id' => 63,
-                'name' => 'Delete Builder',
-                'module' => 'builders',
-                'permission' => 'delete-builder',
-                'api_permission' => 'builder:delete',
-            ],
-
-            // sites module
-            [
-                'id' => 64,
-                'name' => 'Browse site',
-                'module' => 'site',
-                'permission' => 'browse-site',
-                'api_permission' => 'site:browse',
-            ],
-            [
-                'id' => 65,
-                'name' => 'Add site',
-                'module' => 'site',
-                'permission' => 'add-site',
-                'api_permission' => 'site:create',
-            ],
-            [
-                'id' => 66,
-                'name' => 'Edit site',
-                'module' => 'site',
-                'permission' => 'edit-site',
-                'api_permission' => 'site:edit',
-            ],
-            [
-                'id' => 67,
-                'name' => 'Delete site',
-                'module' => 'site',
-                'permission' => 'delete-site',
-                'api_permission' => 'site:delete',
-            ],
         ]);
     }
 }
