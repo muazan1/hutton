aa957f23842b6b9c31519c7fefa79e2ea9718b52
Muazan <muazan.stallyons@gmail.com>
2022-09-23 13:13:43 +0500

    Update WageSheetController.php

--- src/Http/Controllers/Api/V1/WageSheetController.php
+++ src/Http/Controllers/Api/V1/WageSheetController.php
@@ -19,6 +19,8 @@ use App\Models\{User, Role};
 
 use Sty\Hutton\Models\{HsJobs, HuttonUser, Plot, Site, WeeklyWork, DailyWork};
 
+use Carbon\Carbon;
+
 class WageSheetController extends Controller
 {
     public function wageSheet(Request $request)
@@ -26,17 +28,72 @@ class WageSheetController extends Controller
         try {
             $role = Role::where('name', 'joiner')->first();
 
-            $joiners = HuttonUser::with('weeklyWork')
+            $joiners = HuttonUser::with('weeklyWork.dailyWork','weeklyWork.miscWork')
+//                ->whereHas('weeklyWork' , function ($query) {
+//                    $query->where('status','in-progress');
+//                })
                 ->where('role_id', $role->id)
                 ->get();
 
             $meta = User::where('role_id', $role->id)->paginate(10);
 
-            $collection = collect($joiners)->map(function ($item) {
-                dd($item);
+            $joiners = collect($joiners)->map(function ($item) {
+
+                $item->dailyTotal = 0;
+
+                $item->weeklyTotal = 0;
+
+                $item->first = ($item->weeklyWork->where('status','in-progress')->first());
+
+
+
+                if($item->first->dailyWork->count() > 0){
+
+                    $item->dailyTotal += $item->first->dailyWork
+                                            ->where('created_at','>=',Carbon::now()->format('Y-m-d'))
+                                            ->sum('amount');
+                }
+
+                if($item->first->miscWork->count() > 0){
+
+                    $item->dailyTotal += $item->first->miscWork
+                                            ->where('created_at',Carbon::now())
+                                            ->sum('amount');
+                }
+
+
+                if($item->first->dailyWork->count() > 0){
+
+                    $item->weeklyTotal += $item->first->dailyWork->sum('amount');
+                }
+
+                if($item->first->miscWork->count() > 0){
+
+                    $item->weeklyTotal += $item->first->miscWork->sum('amount');
+                }
+
+//                if($item->weeklyWork->count() > 0) {
+//
+//                    foreach ($item->weeklyWork as $value) {
+//
+//                        if($value->dailyWork->count() > 0)
+//                        {
+//                            $item->weeklyTotal += $value->dailyWork->sum('amount');
+//                        }
+//
+//                        if($value->miscWork->count() > 0)
+//                        {
+//                            $item->weeklyTotal += $value->miscWork->sum('amount');
+//                        }
+//
+//                    }
+//                }
+
+
+                return ($item);
             });
 
-            return response()->json([
+             return response()->json([
                 'type' => 'success',
                 'message' => '',
                 'data' => ['joiner' => $joiners, 'meta' => $meta],
