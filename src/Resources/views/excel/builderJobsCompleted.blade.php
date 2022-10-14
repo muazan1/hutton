dce4404e1dfe926da99d7935e74c36a9149f2f22
Muazan <muazan.stallyons@gmail.com>
2022-10-14 18:50:40 +0500

    Update builderJobsCompleted.blade.php

--- src/Resources/views/excel/builderJobsCompleted.blade.php
+++ src/Resources/views/excel/builderJobsCompleted.blade.php
@@ -1,15 +1,15 @@
 <table>
     <tr>
-        <td style="font-weight: bold">
+        <td style="font-weight: bold;width: 100% !important;">
             {{'Site'}}
         </td>
-        <td style="font-weight: bold">
+        <td style="font-weight: bold;width: 100% !important;">
             {{'Joiner'}}
         </td>
-        <td style="font-weight: bold">
+        <td style="font-weight: bold;width: 100% !important;">
             {{'Service'}}
         </td>
-        <td style="font-weight: bold">
+        <td style="font-weight: bold;width: 100% !important;">
             {{'Date & Time'}}
         </td>
 {{--        <td style="font-weight: bold">--}}
@@ -27,17 +27,17 @@
         @foreach($data as $index => $value)
 {{--    {{dd($value->jobjoiner[0])}}--}}
             <tr>
-                <td>
+                <td style="width: 100% !important;">
                     {{ $value->plot->buildingType->site->site_name }}
                 </td>
-                <td>
+                <td style="width: 100% !important;">
                     {{ $value->jobjoiner != NULL ?
                         $value->jobjoiner->first_name.' '.$value->jobjoiner->last_name :'' }}
                 </td>
-                <td>
+                <td style="width: 100% !important;">
                     {{ $value->service->service_name }}
                 </td>
-                <td>
+                <td style="width: 100% !important;">
                     {{ \Carbon\Carbon::parse($value->updated_at)->format('d/m/Y H:i:s A') }}
                 </td>
 {{--                <td>--}}
