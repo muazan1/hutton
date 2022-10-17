9ba20c73e2b042a60f191d8073a413adbabb0abb
Muazan <muazan.stallyons@gmail.com>
2022-10-17 17:01:47 +0500

    Update ChatController.php

--- src/Http/Controllers/Api/V1/ChatController.php
+++ src/Http/Controllers/Api/V1/ChatController.php
@@ -176,7 +176,7 @@ class ChatController extends Controller
 
             $chats = Message::with('admin','joiner','replies')
                                 ->where('admin_id',$admin->id)
-                                ->where('is_read',0)
+//                                ->where('is_read',0)
                                 ->get();
 
             return response()->json([
@@ -323,5 +323,4 @@ class ChatController extends Controller
         }
 
     }
-
 }
